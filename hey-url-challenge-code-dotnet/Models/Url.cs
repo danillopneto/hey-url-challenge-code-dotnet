4f8a361dca311c5355cd0ba12875c4d7b8b245dd
Jeremy Garner <jeremy@fullstacklabs.co>
2021-06-11 17:26:39 -0500

    initial commit

---
+++ hey-url-challenge-code-dotnet/Models/Url.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace hey_url_challenge_code_dotnet.Models
+{
+    public class Url
+    {
+        public Url()
+        {
+        }
+
+        public string ShortUrl { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ hey-url-challenge-code-dotnet/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using hey_url_challenge_code_dotnet.Models;
+
+namespace hey_url_challenge_code_dotnet.ViewModels
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+        }
+
+        public IEnumerable<Url> Urls
+        {
+            get; set;
+        }
+    }
+}
